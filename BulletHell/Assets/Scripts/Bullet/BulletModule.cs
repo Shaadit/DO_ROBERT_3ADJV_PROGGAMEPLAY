caab6bfa2859a01431d1d1df2a276d5f471c8de6
Shaadit <56261900+Shaadit@users.noreply.github.com>
2021-03-23 12:29:16 +0100

    Push Gun + Bullet

---
+++ BulletHell/Assets/Scripts/Bullet/BulletModule.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Bullet
+{
+    public class BulletModule : ScriptableObject
+    {
+        public Transform BulletTransform;
+        public float Speed;
+
+        public void SetSpeed(float bulletSpeed)
+        {
+            this.Speed = bulletSpeed;
+        }
+
+        public BulletModule(float speed, Transform bulletTransform)
+        {
+            this.Speed = speed;
+            this.BulletTransform = bulletTransform;
+        }
+        
+    }
+}
---
+++ BulletHell/Assets/Scripts/Bullet/BulletSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace Bullet
+{
+    public class BulletSystem : IUpdater
+
+    {
+    public void SystemUpdate()
+    {
+        TAccessor<BulletModule> moduleAccessor = TAccessor<BulletModule>.Instance();
+        foreach (var module in moduleAccessor.DisplayListT())
+        {
+            module.BulletTransform.position += Vector3.up * (Time.deltaTime * module.Speed);
+        }
+    }
+    }
+}
