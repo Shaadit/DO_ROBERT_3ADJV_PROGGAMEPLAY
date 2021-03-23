06a7327bce9f678d8ba22ffba8f132414c60e8d3
Shaadit <56261900+Shaadit@users.noreply.github.com>
2021-03-23 16:47:27 +0100

    Push Bullet Boss

---
+++ BulletHell/Assets/Scripts/Bullet/BulletScript.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Bullet;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    private List<ScriptableObject> Modules;
+    public BulletModule bulletComponent;
+
+    public bool shootDown;
+
+    [SerializeField] 
+    private Transform bullet;
+
+    [SerializeField] 
+    private GameObject wall;
+
+    public float speed;
+
+    private void Start()
+    {
+        Vector3 direction = shootDown == false ? Vector3.up : Vector3.down;
+        bulletComponent = new BulletModule(speed, bullet, direction);
+        Modules = new List<ScriptableObject>();
+        Modules.Add(bulletComponent);
+        TAccessor<BulletModule>.Instance().Add(bulletComponent);
+    }
+    
+}
---
+++ BulletHell/Assets/Scripts/Bullet/BulletSystem.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace Bullet
+{
+    public class BulletSystem : IUpdater
+
+    {
+        public void SystemUpdate()
+        {
+            TAccessor<BulletModule> moduleAccessor = TAccessor<BulletModule>.Instance();
+            foreach (var module in moduleAccessor.DisplayListT())
+            {
+                module.bulletTransform.position += module.direction * (Time.deltaTime * module.speed);
+
+            }
+
+        }
+    }
+}
